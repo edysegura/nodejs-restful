f501767bcec674bd63998249a8b449511e07d93c
Edy Segura <edysegura@gmail.com>
2015-10-03 17:09:16 -0300

    Completed REST API for Tasks

--- app/taskApi.js
+++ app/taskApi.js
@@ -6,6 +6,10 @@ var
     db = {},
     sequence = 0;
 
+var notFound = function(res) {
+    res.status(404).send('Not found!');
+};
+
 router.get('/', function(req, res) {
     res.json(db);
 });
@@ -16,8 +20,57 @@ router.get('/:id', function(req, res) {
         res.json(task);
     }
     else {
-        res.status(404).send('Not found!');
+        notFound(res);
     }
 });
+
+router.post('/', function(req, res) {
+    var newTask = {
+        id: ++sequence,
+        done: req.body.done || false,
+        description: req.body.description
+    };
+    
+    db[newTask.id] = newTask;
+    
+    res.status(201).json(newTask);
+});
+
+router.put('/:id', function(req, res) {
+    var task = db[req.params.id];
+    if(task) {
+        task.done = req.body.done != null ? req.body.done : false;
+        task.description = req.body.description || task.description;
+        res.json(task);
+    }
+    else {
+        notFound(res);
+    }
+});
+
+router.delete('/:id', function(req, res) {
+    var task = db[req.params.id];
+    if(task) {
+        delete db[req.params.id];
+        res.send('Task deleted');
+    }
+    else {
+        notFound(res);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
