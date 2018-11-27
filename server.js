e475fda5b4ce410f3d1b4fad7ce49a6482e92453
Edy Segura <edysegura@gmail.com>
2018-11-26 23:43:54 -0200

    Refactoring server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 // Dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
 // Express configuration
-var app = express();
-app.use(bodyParser.json());
-app.use(express.static('public'));
+const app = express()
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
-//welcome
-app.get('/', function(req, res) {
-    res.redirect('./index.html');
-});
+// Welcome
+app.get('/', (request, response) => {
+  response.redirect('./index.html')
+})
 
 // Task API
-app.use('/api/tasks', require('./app/taskApi'));
+app.use('/api/tasks', require('./app/taskApi'))
 
-var server = app.listen(8080, function() {
-    console.log("Server running at http://localhost:8080");
-});
+app.listen(8080, () => {
+  console.log('Server running at http://localhost:8080')
+})
