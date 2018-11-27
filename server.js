a0d98b1adf41e4af60f3dfc3344352cc93b093fd
Edy Segura <edysegura@gmail.com>
2018-11-26 23:44:51 -0200

    Removing unnecessary comments

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-// Dependencies
 const express = require('express')
 const bodyParser = require('body-parser')
 
-// Express configuration
 const app = express()
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-// Welcome
 app.get('/', (request, response) => {
   response.redirect('./index.html')
 })
 
-// Task API
 app.use('/api/tasks', require('./app/taskApi'))
 
 app.listen(8080, () => {
