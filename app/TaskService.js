b1040aec61f3938aed26c2d35fd5f1bdbb966985
Edy Segura <edysegura@gmail.com>
2019-11-25 09:55:22 -0300

    Update TaskService.js

--- app/TaskService.js
+++ app/TaskService.js
@@ -1,43 +1,57 @@
 const db = {}
-let sequence = 0
+let sequence = 0;
 
 class TaskService {
 
-  static save(task) {
+  static add(newTask) {
     return new Promise((resolve) => {
-      task.id = ++sequence
-      db[task.id] = task
-      resolve(task)
-    })
+      const task = {
+        id: ++sequence,
+        done: newTask.done || false,
+        description: newTask.description
+      };
+      db[task.id] = task;
+      resolve(task);
+    });
+  }
+
+  static getAll() {
+    const toArray = key => db[key];
+    return new Promise((resolve) => {
+      const tasks = Object.keys(db).map(toArray);
+      resolve(tasks);
+    });
   }
 
   static getById(id) {
     return new Promise((resolve) => {
-      resolve(db[id])
-    })
+      resolve(db[id]);
+    });
   }
 
-  static getAll() {
-    const toArray = key => {
-      return db[key]
-    }
-
-    return new Promise((resolve) => {
-      const tasks = Object.keys(db).map(toArray)
-      resolve(tasks)
+  static update(taskId, updatedTask) {
+    return new Promise(async (resolve) => {
+      const task = await TaskService.getById(taskId);
+      if(task) {
+        const hasValue = updatedTask.done != null;
+        task.done = hasValue ? updatedTask.done : task.done;
+        task.description = updatedTask.description || task.description;
+        resolve(task);
+      }
+      resolve(null);
     })
   }
 
   static delete(id) {
     return new Promise((resolve) => {
-      let task = db[id]
+      const task = db[id];
       if(task) {
-        delete db[id]
-        resolve(true)
+        delete db[id];
+        resolve(true);
       }
-      resolve(false)
-    })
+      resolve(false);
+    });
   }
 }
 
-module.exports = TaskService
+module.exports = TaskService;
