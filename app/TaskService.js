183fe1fc6bbf52f67a20443a8bb9868b04072177
Edy Segura <edysegura@gmail.com>
2019-12-26 15:21:54 -0300

    Remove semi-colon from TaskService

--- app/TaskService.js
+++ app/TaskService.js
@@ -1,5 +1,5 @@
 const db = {}
-let sequence = 0;
+let sequence = 0
 
 class TaskService {
 
@@ -9,49 +9,49 @@ class TaskService {
         id: ++sequence,
         done: newTask.done || false,
         description: newTask.description
-      };
-      db[task.id] = task;
-      resolve(task);
-    });
+      }
+      db[task.id] = task
+      resolve(task)
+    })
   }
 
   static getAll() {
-    const toArray = key => db[key];
+    const toArray = key => db[key]
     return new Promise((resolve) => {
-      const tasks = Object.keys(db).map(toArray);
-      resolve(tasks);
-    });
+      const tasks = Object.keys(db).map(toArray)
+      resolve(tasks)
+    })
   }
 
   static getById(id) {
     return new Promise((resolve) => {
-      resolve(db[id]);
-    });
+      resolve(db[id])
+    })
   }
 
   static update(taskId, updatedTask) {
     return new Promise(async (resolve) => {
-      const task = await TaskService.getById(taskId);
-      if(task) {
-        const hasValue = updatedTask.done != null;
-        task.done = hasValue ? updatedTask.done : task.done;
-        task.description = updatedTask.description || task.description;
-        resolve(task);
+      const task = await TaskService.getById(taskId)
+      if (task) {
+        const hasValue = updatedTask.done != null
+        task.done = hasValue ? updatedTask.done : task.done
+        task.description = updatedTask.description || task.description
+        resolve(task)
       }
-      resolve(null);
+      resolve(null)
     })
   }
 
   static delete(id) {
     return new Promise((resolve) => {
-      const task = db[id];
-      if(task) {
-        delete db[id];
-        resolve(true);
+      const task = db[id]
+      if (task) {
+        delete db[id]
+        resolve(true)
       }
-      resolve(false);
-    });
+      resolve(false)
+    })
   }
 }
 
-module.exports = TaskService;
+module.exports = TaskService
